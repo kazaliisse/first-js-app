d36b3d67d773bfb24210324e362541fc9d600cdb
noor <noorhafowbare@gmail.com>
2024-06-11 11:24:19 +0300

    js-app

---
+++ first app.js
@@ -0,0 +1,8 @@
+// user input
+food = Number(prompt('How much was food amount'))
+tipPercentage = Number(prompt('tip %?') / 100)
+tipAmount = food * tipPercentage
+total = food + tipAmount
+
+// console.log(tipAmount)
+console.log(total)
